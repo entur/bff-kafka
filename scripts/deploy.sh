ea3e2b8c87e7d3463bdf523ec5c41a3530f300f5
Joakim Tysseng <joakim.tysseng@entur.org>
2021-04-20 11:07:00 +0200

    Added initial stuff from bff-search

---
+++ scripts/deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$ENTUR_DEPLOY_SLACK_WEBHOOK" ] ; then
+ echo
+ echo "👮‍♀️ Stop there! Could not find the Slack webhook URL. Please make sure this variable is exported:"
+ echo
+ echo "  ENTUR_DEPLOY_SLACK_WEBHOOK"
+ echo
+ exit 1
+fi
+
+function deploy {
+    ENV="${1:-dev}"
+
+    if ! [[ "$ENV" =~ ^(dev|staging|prod|beta)$ ]]; then
+        echo -e "🙈 Invalid ENV: $ENV\n"
+        exit 1
+    fi
+
+    echo " 🧵  Linting ..."
+    npm run lint
+
+    echo " 🚢 Deploying BFF Kafka to $ENV ..."
+    npm run build:$ENV && gcloud app deploy app-$ENV.yaml --project=entur-$ENV --quiet
+
+    echo " 💬 Posting message to Slack ..."
+    slack_message $ENV
+}
+
+function slack_message {
+    ENV=$1
+    BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
+    BRANCH="(unnamed branch)"
+    BRANCH=${BRANCH##refs/heads/}
+
+    COMMIT="$(git rev-parse --short HEAD)"
+
+    if [[ $(git diff --stat) != '' ]]; then
+        COMMIT_MSG="(\`$COMMIT\` DIRTY :poop:)"
+    else
+        COMMIT_MSG="(\`$COMMIT\`)"
+    fi
+
+    curl -X POST \
+        --data-urlencode "payload={\"channel\": \"#team-app-build\", \"username\": \"BFF Kafka deployed to $ENV\", \"text\": \"\`$USER\` deployed *BFF Search* to :$ENV: from branch \`$BRANCH\` $COMMIT_MSG\", \"icon_emoji\": \":mag:\"}" \
+        "$ENTUR_DEPLOY_SLACK_WEBHOOK"
+}
+
+ENV_ARGS="${@:-dev}"
+for ENV_ARG in $ENV_ARGS
+do
+    deploy "$ENV_ARG"
+done
---
+++ scripts/environment.ts
@@ -0,0 +1,68 @@
+import { join } from 'path'
+import { promisify } from 'util'
+import fs from 'fs'
+
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const mkdir = promisify(fs.mkdir)
+
+const [, , ENV = 'dev', ...args] = process.argv
+const ENV_FILE = join(__dirname, `../.env.${ENV}`)
+
+void createConfigFile()
+
+if (args.includes('--with-types')) {
+    createTypeDefinition().catch((error) => {
+        console.error('Failed creating type definition', error)
+    })
+}
+
+if (args.includes('--watch')) {
+    // eslint-disable-next-line fp/no-mutating-methods
+    fs.watch(ENV_FILE, () => {
+        void createConfigFile()
+    })
+}
+
+async function readEnvFile(filePath: string): Promise<Record<string, string>> {
+    const content = await readFile(filePath, 'utf-8')
+    const lines = content.trim().split('\n')
+
+    return lines.reduce((map, line) => {
+        const [key, value] = line.split('=').map((s) => s.trim())
+        if (!key) return map
+        return {
+            ...map,
+            [key]: value,
+        }
+    }, {})
+}
+
+async function createTypeDefinition(): Promise<void> {
+    const envConfig = await readEnvFile(ENV_FILE)
+    const format = (key: string): string => `export const ${key}: string`
+
+    const content = `${Object.keys(envConfig).map(format).join('\n')}
+`
+
+    return writeFile(join(__dirname, '../src/config.d.ts'), content)
+}
+
+async function createConfigFile(): Promise<void> {
+    try {
+        const envConfig = await readEnvFile(ENV_FILE)
+        const format = ([key, value]: [string, string]): string =>
+            `exports.${key} = "${value}";`
+
+        const content = `"use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+
+    ${Object.entries(envConfig).map(format).join('\n')}
+    `
+        await mkdir(join(__dirname, '..', 'dist'), { recursive: true })
+        await writeFile(join(__dirname, '../dist/config.js'), content)
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
