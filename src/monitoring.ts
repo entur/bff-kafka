d6d209edec04fe73f73de19913fc27fba625abd3
Joakim Tysseng <joakim.tysseng@entur.org>
2023-11-09 09:01:09 +0100

    Added debug logging to consumer.heartbeat to see if it goes missing

--- src/monitoring.ts
+++ src/monitoring.ts
@@ -1,8 +1,11 @@
+import logger from './logger.js'
+
 const MINUTE = 60000
 const threshold = 3 * MINUTE
 let lastHeartbeat = Date.now()
 export const updateLastHeartbeat = (): void => {
     lastHeartbeat = Date.now()
+    logger.debug('consumer.heartbeat received', { lastHeartbeat })
 }
 
 export const isHeartBeating = (): boolean => Date.now() - lastHeartbeat < threshold
